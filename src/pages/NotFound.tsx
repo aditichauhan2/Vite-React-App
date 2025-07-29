8aab7ced4908f9134256b2e7bcd9e83a0e79cd81
aditichauhan2 <aditi.chauhan@card91.io>
2025-07-29 11:05:39 +0530

    fixed redirect ink

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
         <h1 className="text-5xl font-bold text-red-600">404</h1>
         <p className="text-xl mt-4 text-gray-700">Page Not Found</p>
         <Link
-          to={isAuthenticated ? '/' : '/login'}
+          to='/'
           className="text-blue-600 hover:underline mt-4 block"
         >
           Go back to Home
